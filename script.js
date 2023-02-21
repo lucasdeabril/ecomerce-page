0fa290a4ec684aeaa11071cb9384c73e58825784
lucasdeabril <118875218+lucasdeabril@users.noreply.github.com>
2023-02-21 02:48:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,33 +3,43 @@ var fotoes=document.getElementById('fotoes')
 var i = 1
 function mudafoto (n) {
    foto.src='image-product-'+n+'.jpg'
-    // if(i<4){
-    //     i++
-    // foto.src='image-product-'+i+'.jpg'
-    // }
-    // else{
-    //     i=1
-    //     foto.src='image-product-'+i+'.jpg'
-    // }
     
+}
+var m =document.getElementById('cinza')
+function menu() {
+    
+    m.style.display='inline-block'
+}
+function sumir(){
+    m.style.display='none'
 }
 function mudafoto2 (n) {
     let ind = n
     fotoes.src='image-product-'+n+'.jpg'
-
+    i=n
+    // window.alert(i)
 
 }
 function voltafoto(){
-    if(fotoes.src = "image-product-1.jpg"){
-        fotoes.src="image-product-4.jpg"
+    if (i==1){
+        i=4
+        fotoes.src='image-product-'+i+'.jpg'
+        
+    }else{
+        var v = i - 1
+    fotoes.src='image-product-'+v+'.jpg'
+    i=v
     }
-    if(fotoes.src = "image-product-4.jpg"){
-        fotoes.src="image-product-3.jpg"
-    }
-    if(fotoes.src = "image-product-3.jpg"){
-        fotoes.src="image-product-2.jpg"
-    }
-    if(fotoes.src = "image-product-2.jpg"){
-        fotoes.src="image-product-1.jpg"
-    }
+    
+}
+function avancafoto(){
+    if (i==4){
+        i=1
+        fotoes.src='image-product-'+i+'.jpg'
+        
+    }else{
+        var v = i + 1
+    fotoes.src='image-product-'+v+'.jpg'
+    i=v
+    }    
 }
